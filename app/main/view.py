e29dd2f8ddf6ef5401ba65de2394f317fdb53ec7
fish01 <zeng_fsh@163.com>
2017-10-11 21:53:11 +0800

    flask auth

--- app/main/view.py
+++ app/main/view.py
@@ -1,6 +1,14 @@
 from . import main
-
+from .form import NameForm
+from flask import render_template,session
 
 @main.route('/',methods=['GET','POST'])
 def index():
-    return "<h1>index page</h1>"
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name']=form.name.data
+        return "<h1>hahaha</h1>"
+    return render_template('index.html',
+                           form=form,
+                           name=session.get('name'),
+                           know=session.get('know'))
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 
-
+import os
 from app import create_app
 
 
 if __name__ == '__main__':
-    app = create_app()
+    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
     app.run(debug=True)
 
 
